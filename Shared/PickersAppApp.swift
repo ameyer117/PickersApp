97e20ceff10ee6f1cab12b895bf8daf03ccbd593
Alec Meyer <11meyal@gmail.com>
2022-01-14 15:40:06 -0500

    Initial Commit

---
+++ Shared/PickersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickersAppApp.swift
+//  Shared
+//
+//  Created by Alec Meyer on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PickersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
